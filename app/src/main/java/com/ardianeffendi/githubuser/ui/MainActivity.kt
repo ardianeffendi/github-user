6c6f223ae122a9e3907c3440d97a36354a0635f4
Ardian Effendi <ardian.effendi21@gmail.com>
2020-07-20 14:10:21 +0800

    insert dependencies

---
+++ app/src/main/java/com/ardianeffendi/githubuser/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ardianeffendi.githubuser.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ardianeffendi.githubuser.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
